9dbb2dbba677e6cd2fb16d1eada437d23a47bfeb
Husaini <husainirudin00@icloud.com>
2023-11-24 15:38:54 +0800

    folder structure 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:pc_tech/modules/pages/MFA.dart';
+import 'package:pc_tech/modules/auth/MultiFactorAuth/MFA.dart';
 import 'package:pc_tech/modules/home/homepage.dart';
 import 'package:pc_tech/modules/auth/login/login.dart';
 import 'package:pc_tech/modules/auth/register/register.dart';
